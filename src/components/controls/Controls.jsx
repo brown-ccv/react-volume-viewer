e15e7a77fb1684b0c5049c9563cf1b0acc86dccb
RobertGemmaJr <rgemma.jr@gmail.com>
2021-11-15 12:31:38 -0500

    style: center controls with a max-height

--- src/components/controls/Controls.jsx
+++ src/components/controls/Controls.jsx
@@ -36,13 +36,14 @@ function Controls(props) {
 const Wrapper = styled.div`
   position: absolute;
   overflow: auto;
-  top: 25px;
   left: 25px;
-  right: 25px;
+  top: 25px;
   bottom: 25px;
+  margin: auto 0;
   width: 300px;
+  height: fit-content;
+  max-height: calc(100% - 80px);
   padding: 15px;
-
   background-color: white;
   border-radius: 5%;
 `;
