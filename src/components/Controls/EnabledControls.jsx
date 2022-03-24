93c3cdc04be05785606abf7a693983490c20c1d9
RobertGemmaJr <rgemma.jr@gmail.com>
2022-03-24 15:20:04 -0400

    chore: update packages

--- src/components/Controls/EnabledControls.jsx
+++ src/components/Controls/EnabledControls.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-import visible from "../../images/visible.svg"
-import visibleOff from "../../images/visible_off.svg"
-import styled from "styled-components";
+import visible from "../../images/visible.svg";
+import visibleOff from "../../images/visible_off.svg";
+// import styled from "styled-components";
 
 function EnabledControls({ enabled, modelIdx, setModels }) {
   return (
-    <IconCheckbox
+    <img
+      src={enabled ? visible : visibleOff}
+      alt={enabled ? "visible" : "visible off"}
       onClick={(e) => {
         e.stopPropagation();
         setModels((models) => [
@@ -19,16 +19,8 @@ function EnabledControls({ enabled, modelIdx, setModels }) {
           ...models.slice(modelIdx + 1),
         ]);
       }}
-    >
-      {enabled ? (
-        <img src={visible} alt="visible"/>
-      ) : (
-        <img src={visibleOff} alt="visible off"/>
-      )}
-    </IconCheckbox>
+    />
   );
 }
-
-const IconCheckbox = styled.span``;
 
 export default EnabledControls;
