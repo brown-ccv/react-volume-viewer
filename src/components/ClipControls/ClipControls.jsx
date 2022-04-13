7a872d72c65c62b5b90e27867d4693c847982e29
RobertGemmaJr <rgemma.jr@gmail.com>
2022-04-13 10:38:55 -0400

    style: add slider marks at 0, 0.5, and 1

--- src/components/ClipControls/ClipControls.jsx
+++ src/components/ClipControls/ClipControls.jsx
@@ -7,6 +7,12 @@ import "rc-slider/assets/index.css";
 import Section from "../Section";
 import { SLIDER_RANGE } from "../../constants";
 
+const marks = {
+  0: " ",
+  0.5: " ",
+  1: " ",
+}
+
 // TODO: Slider range should be 0-1000 with a step of 1 (changes needed in AframeScene)
 function ClipControls({ sliders, setSliders }) {
   function handleChange(axis, val) {
@@ -23,11 +29,12 @@ function ClipControls({ sliders, setSliders }) {
           <Axis> {axis.toUpperCase()} Axis</Axis>
           <Slider
             range
+            marks={marks}
             min={SLIDER_RANGE.min}
             max={SLIDER_RANGE.max}
             step={0.001}
-            value={sliders[axis]}
             allowCross={false}
+            value={sliders[axis]}
             onChange={(val) => handleChange(axis, val)}
           />
         </SliderGroup>
