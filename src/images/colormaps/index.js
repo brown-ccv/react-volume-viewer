5ad4d7cfca474e51bf2263ea38573cdbe77033d5
RobertGemmaJr <rgemma.jr@gmail.com>
2022-06-23 16:22:42 -0400

    refactor: build colormaps in seperate folder

---
+++ src/images/colormaps/index.js
@@ -0,0 +1,55 @@
+import algae from "./algae.png";
+import amp from "./amp.png";
+import balance from "./balance.png";
+import curl from "./curl.png";
+import deep from "./deep.png";
+import delta from "./delta.png";
+import dense from "./dense.png";
+import diff from "./diff.png";
+import grayscale_reverse from "./grayscale_reverse.png";
+import grayscale from "./grayscale.png";
+import haline from "./haline.png";
+import ice from "./ice.png";
+import matter from "./matter.png";
+import natural from "./natural.png";
+import oxy from "./oxy.png";
+import phase from "./phase.png";
+import rain from "./rain.png";
+import rgb from "./rgb.png";
+import solar from "./solar.png";
+import speed from "./speed.png";
+import tarn from "./tarn.png";
+import tempo from "./tempo.png";
+import thermal from "./thermal.png";
+import topo from "./topo.png";
+import turbid from "./turbid.png";
+
+const colormaps = {
+  Algae: { name: "algae", path: algae },
+  Amp: { name: "amp", path: amp },
+  Balance: { name: "balace", path: balance },
+  Curl: { name: "curl", path: curl },
+  Deep: { name: "deep", path: deep },
+  Delta: { name: "delta", path: delta },
+  Dense: { name: "dense", path: dense },
+  Diff: { name: "diff", path: diff },
+  Grayscale_reverse: { name: "grayscale_reverse", path: grayscale_reverse },
+  Grayscale: { name: "grayscale", path: grayscale },
+  Haline: { name: "haline", path: haline },
+  Ice: { name: "ice", path: ice },
+  Matter: { name: "matter", path: matter },
+  Natural: { name: "natural", path: natural },
+  Oxy: { name: "oxy", path: oxy },
+  Phase: { name: "phase", path: phase },
+  Rain: { name: "rain", path: rain },
+  Rgb: { name: "rgb", path: rgb },
+  Solar: { name: "solar", path: solar },
+  Speed: { name: "speed", path: speed },
+  Tarn: { name: "tarn", path: tarn },
+  Tempo: { name: "tempo", path: tempo },
+  Thermal: { name: "thermal", path: thermal },
+  Topo: { name: "topo", path: topo },
+  Turbid: { name: "turbid", path: turbid },
+};
+
+export default colormaps;
